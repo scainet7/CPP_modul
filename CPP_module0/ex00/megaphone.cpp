368cc725fdfac3b2212fb6d9c3bcc345e99c6ca4
INSAF GATAULLIN <86107897+scainet7@users.noreply.github.com>
2022-11-05 13:29:59 +0300

    V0.1
    add module_00 + subjects

---
+++ CPP_module0/ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snino <snino@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/01 19:02:30 by snino             #+#    #+#             */
+/*   Updated: 2022/11/01 19:02:30 by snino            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int i;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	++argv;
+	while (*argv)
+	{
+		std::string str = *argv;
+		i = 0;
+		while (str[i])
+		{
+			std::cout << (char)toupper(str[i]);
+			i++;
+		}
+		argv++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
+
